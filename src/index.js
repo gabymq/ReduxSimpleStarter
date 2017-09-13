dbdad4ad3e9c111e2ce74e6b756367bd515e95a3
gabymq <gmorenogaby@gmail.com>
2017-09-12 20:01:20 -0500

    Section4 Session 1-13 React-Redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-// Create a new component. This component should produce
-// some HTML
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/app';
+import reducers from './reducers';
 
-//
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const domSelector = document.querySelector('.container');
+const reactApp = (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render( reactApp , domSelector );
