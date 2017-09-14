d47e8f515c0d1865d5e0784dac2c888cc6356f4b
gabymq <gmorenogaby@gmail.com>
2017-09-13 19:42:54 -0500

    Section5 Session 1 to 10 51-60

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const domSelector = document.querySelector('.container');
+const reactApp = (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render( reactApp , domSelector );
+
+
 // Create a new component. This component should produce
 // some HTML
 
