8a31927552d0f1277ef92a97db80884262dbd840
gabymq <gmorenogaby@gmail.com>
2017-09-18 18:40:31 -0500

    Section6 session 114-124

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+               <Route path="/" component={PostsIndex} />
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
+
+
+
+
+// Create a new component. This component should produce
+// some HTML
+
+
+
+
+
 // Create a new component. This component should produce
 // some HTML
 
