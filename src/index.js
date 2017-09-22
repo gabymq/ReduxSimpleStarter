1af866748e6760e6e3e4d67bf64162d19c39c08e
gabymq <gmorenogaby@gmail.com>
2017-09-22 14:31:15 -0500

    Refactor Session 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const domSelector = document.querySelector('.container');
+const reactApp = (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render( reactApp , domSelector );
+
+
+
 // Create a new component. This component should produce
 // some HTML
 
