b513e32ef5236d75470d338681fbf26747d1d601
gabymq <gmorenogaby@gmail.com>
2017-09-28 14:11:50 -0500

    Section7.8 BrowserHistory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-// Create a new component. This component should produce
-// some HTML
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createSore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
 
-//
+import App from './components/app';
+import UserList from './components/child';
+import reducers from './reducers';
+
+const createStoreWhitMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider stores={createStoreWhitMiddleware(reducers)}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <Route path="users" component={UsersList} />
+            </Route>
+        </Router>
+    </Provider>
+    , document.querySelector('.container'));
+        
